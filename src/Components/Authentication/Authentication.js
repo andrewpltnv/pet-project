189f50248939073515c21fb3ca977c0537cdcb11
Andrey <andrewpltnv@gmail.com>
2020-03-02 18:49:29 +0200

    Correct login

--- src/Components/Authentication/Authentication.js
+++ src/Components/Authentication/Authentication.js
@@ -1,41 +1,37 @@
 import React, {useState} from "react";
 import "./Authentication.css";
+const axios = require('axios').default;
 
 const Authentication = (props) => {
 
   const [name, setName] = useState('');
-  const [pass, setPass] = useState('');
 
   const handleNameChange = (event) => {
     const temp = (event.target.value);
     if (typeof temp !== 'undefined') setName(temp);
   };
-  const handlePassChange = (event) => {
-    const temp = (event.target.value);
-    if (typeof temp !== 'undefined') setPass(temp);
-  };
 
   const handleSubmit = (event) => {
     localStorage.setItem('name', name.toString());
-    localStorage.setItem('pass', pass.toString());
+
+    const response = axios.post(
+      'http://localhost:3001/register',
+      { name: name}
+    );
+    console.log(response.data);
+    document.location.href = "";
+    event.preventDefault();
   };
 
   return(
     <div id="user--slot">
-      <form target="_self" action="" onSubmit={handleSubmit} autoComplete="on" className="main--form">
+      <form target="_self" action="/register" method="post" onSubmit={handleSubmit} autoComplete="on" className="main--form">
           <input name="name"
                  type="text"
                  maxLength="8"
                  value={name}
                  placeholder="Name"
                  onChange={handleNameChange}
-          />
-          <input name="password"
-                 type="password"
-                 maxLength="8"
-                 value={pass}
-                 placeholder="Password"
-                 onChange={handlePassChange}
           />
           <input type="submit"
                  value="Log in"
